61dbfcd271f0307e280b7b399decee6c0140ab41
Jeroen Meetsma <jeroen@ijsberg.nl>
2012-12-27 17:44:35 +0100

    changes for appserver release

---
+++ src/main/java/org/ijsberg/iglu/server/http/filter/WebAppEntryPoint.java
@@ -0,0 +1,546 @@
+/* =======================================================================
+ * Copyright (c) 2003-2010 IJsberg Automatisering BV. All rights reserved.
+ * Redistribution and use of this code are permitted provided that the
+ * conditions of the Iglu License are met.
+ * The license can be found in org.ijsberg.iglu.StandardApplication.java
+ * and is also published on http://iglu.ijsberg.org/LICENSE.
+ * =======================================================================
+ */
+package org.ijsberg.iglu.server.http.filter;
+
+import org.ijsberg.iglu.access.*;
+import org.ijsberg.iglu.logging.Level;
+import org.ijsberg.iglu.logging.LogEntry;
+import org.ijsberg.iglu.server.http.servlet.ServletRequestAlreadyRedirectedException;
+import org.ijsberg.iglu.util.collection.CollectionSupport;
+import org.ijsberg.iglu.util.http.ServletSupport;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Cookie;
+import java.io.IOException;
+import java.util.*;
+
+
+/**
+ * This filter can be used as point of entry into a web application.
+ * This filter performs several tasks:
+ * <li>It binds a request as soon as a http-request enters the system.</li>
+ * <li>In the process it may also resolve a related session, referenced by a session token (stored client-side).</li>
+ * <li>It releases the request as soon as the http-request has been processed.</li>
+ * <li>It may perform role based access control.</li>
+ * <li>It provides exception handling.</li>
+ * Servlets and JSP-pages that are part of the web application will thus
+ * be provided with a registered request that can be retrieved throughout the course of
+ * the request by invoking <emph>application.getCurrentRequest()</emph>.
+ * <p/>
+ * Specify a servlet init parameter 'realm_id=....' to indicate which realm is entered here.
+ * <p/>
+ * Make sure to filter on dynamic content only, to avoid unnecessary overhead of request administration.
+ *
+ * If an URL-pattern matches multiple filters, the request will pass these
+ * filters in a chain. Note that the first filter creates the request and
+ * determines which realm is entered.  
+ */
+public class WebAppEntryPoint implements Filter, EntryPoint
+{
+	private AccessManager accessManager;
+
+	public static final String SESSION_TOKEN_KEY = "IGLU_SESSION_TOKEN";
+	private String USER_ID_KEY = "IGLU_USER_ID";
+
+//	protected Realm realm;
+//	protected Application application;
+	protected String filterName;
+
+	private boolean syncUserPrefs;
+	private int userPrefsMaxAge = -1;
+
+	private HashMap exceptionPages = new HashMap();
+//	private List exceptionsHandled = new ArrayList();
+	//debug mode
+	private boolean printUnhandledExceptions = true;
+
+	private ThreadLocal httpRequest = new ThreadLocal();
+	private ThreadLocal httpResponse = new ThreadLocal();
+
+	private HashMap securityConstraints = new HashMap();
+
+	private boolean loggingEnabled;
+	private String exceptionPagesSectionId;
+
+	private static class ExceptionHandlingSettings
+	{
+		public String redirectPage;
+		public int loglevel;
+
+
+
+		public ExceptionHandlingSettings(String redirectPage, int loglevel)
+		{
+			this.redirectPage = redirectPage;
+			this.loglevel = loglevel;
+		}
+	}
+
+
+	public void setAccessManager(AccessManager accessManager) {
+		this.accessManager = accessManager;
+	}
+
+
+	/**
+     *
+     */
+    public WebAppEntryPoint()
+	{
+		super();
+	}
+
+	/**
+	 * Notification of a session registration event.
+	 *
+	 * @param session
+	 */
+	public void onSessionUpdate(Request currentRequest, Session session)
+	{
+		HttpServletResponse response = (HttpServletResponse)httpResponse.get();
+		storeSessionDataInCookie(SESSION_TOKEN_KEY, session.getToken(), response);
+		if(session.getUser(/*realm.getId()*/) != null)
+		{
+			storeSessionDataInCookie(USER_ID_KEY, session.getUser(/*realm.getId()*/).getId(), response);
+		}
+		else
+		{
+			storeSessionDataInCookie(USER_ID_KEY, null, response);
+		}
+	}
+
+	/**
+	 * Notification of a session destruction.
+	 *
+	 * @param session
+	 */
+	public void onSessionDestruction(Request currentRequest, Session session)
+	{
+		HttpServletResponse response = (HttpServletResponse)httpResponse.get();
+		storeSessionDataInCookie(SESSION_TOKEN_KEY, null, response);
+		storeSessionDataInCookie(USER_ID_KEY, null, response);
+	}
+
+    /**
+     *
+     * @param currentRequest
+     * @param properties
+     */
+    public void exportUserSettings(Request currentRequest, Properties properties)
+	{
+		HttpServletResponse response = (HttpServletResponse)httpResponse.get();
+		ServletSupport.exportCookieValues(response, properties, "/", userPrefsMaxAge);
+	}
+
+
+	/**
+	 * Stores session id in the root of a cookie
+	 * The cookie will expire as soon as the browser closes
+	 *
+	 * @param key
+	 * @param value
+	 * @param response
+	 */
+	public static void storeSessionDataInCookie(String key, String value, ServletResponse response)
+	{
+		Cookie cookie = new Cookie(key, value);
+		cookie.setPath("/");
+		cookie.setMaxAge(-1);//expire when browser closes
+		((HttpServletResponse) response).addCookie(cookie);
+	}
+
+	/**
+	 * Retrieves session id from cookie
+	 *
+	 * @param request
+	 * @return
+	 */
+/*	public static String getSessionTokenFromCookie(ServletRequest request)
+	{
+		return ServletSupport.getCookieValue(request, SESSION_TOKEN_KEY);
+	}
+*/
+    /**
+     *
+     * @param currentRequest
+     * @param properties
+     */
+    public void importUserSettings(Request currentRequest, Properties properties)
+	{
+		ServletRequest request = (ServletRequest)httpRequest.get();
+		ServletSupport.importCookieValues(request, properties);
+	}
+
+
+    /**
+     *
+     * @return
+     */
+  /*  public Realm getRealm()
+	{
+		return realm;
+	}*/
+
+	//TODO document init params
+	/**
+	 * @param conf
+	 * @throws ServletException
+	 */
+	public final void init(FilterConfig conf) throws ServletException
+	{
+		filterName = conf.getFilterName();
+	/*	Request request = null;
+
+		obtainApplicationReference(conf);
+		//obtain realm from servlet init parameters
+		obtainRealmReference(conf);
+
+		System.out.println(new LogEntry("initializing filter " + filterName + " (acting as entry point)"));
+
+		USER_ID_KEY = "IGLU_" + realm.getId() + "_USER_ID";
+
+		try
+		{
+			//bind request to provide context for the following calls
+//			request = application.bindRequest(this);
+
+			initializeSettings(conf);
+		}
+		finally
+		{
+/ *			if(request != null)
+			{
+				application.releaseRequest();
+			}* /
+		}      */
+	}
+
+/*	private void obtainApplicationReference(FilterConfig conf)
+	{
+		application = (Application)conf.getServletContext().getAttribute("application");
+
+		if (application == null)
+		{
+			throw new ConfigurationException("application could not obtained from servlet context:" +
+					" make sure the Iglu application is loaded before filter '"
+					+ filterName + "' is initialized");
+		}
+	} */
+
+	private void initializeSettings(FilterConfig conf)
+			throws ServletException
+	{
+		String syncUserPrefsStr = conf.getInitParameter("sync_user_prefs");
+		syncUserPrefs = syncUserPrefsStr != null ? Boolean.valueOf(syncUserPrefs) : false;
+
+		String userPrefsMaxAgeStr = conf.getInitParameter("user_prefs_max_age");
+		userPrefsMaxAge = userPrefsMaxAgeStr != null ? Integer.valueOf(userPrefsMaxAge) : 0;
+
+/*		String loggingEnabledStr = conf.getInitParameter("logging_enabled");
+		loggingEnabled = new GenericValue(loggingEnabledStr).toBoolean(loggingEnabled).booleanValue();
+
+		exceptionPagesSectionId = conf.getInitParameter("exception_pages_section_id");
+		if(exceptionPagesSectionId == null)
+		{
+			exceptionPagesSectionId = realm.getId() + '_' + filterName + "_exception_pages";
+		}
+		initializeExceptionPages();
+		//role based access control settings
+		String securityConstraintsSectionId = conf.getInitParameter("security_constraints_section_id");
+		if(securityConstraintsSectionId == null)
+		{
+			securityConstraintsSectionId = realm.getId() + '_' + filterName + "_security_constraints";
+		}*/
+/*		PropertyBundle constraints = application.getConfigurationSection(securityConstraintsSectionId);
+		if(constraints != null)
+		{
+			Iterator i = constraints.getSubsections().iterator();
+			while(i.hasNext())
+			{
+				PropertyBundle constraint = (PropertyBundle)i.next();
+				securityConstraints.put(constraint.getValue("url_pattern").toString("/*"), constraint.getValue("roles").toList());
+			}
+		} */
+	}
+
+/*	private void initializeExceptionPages()
+			throws ServletException
+	{
+		PropertyBundle exceptionPagesSection = application.getConfigurationSection(exceptionPagesSectionId);
+		exceptionPagesSection.setDescription("declares handling in the form of:\n" +
+				"remainingexceptions.class=java.lang.Exception\n" +
+				"remainingexceptions.page=error.jsp\n" +
+				"remainingexceptions.loglevel=CRITICAL\n" +
+				"Note: order matters");
+//		if(exceptionPagesSection != null)
+//	{
+		Iterator defIterator = exceptionPagesSection.getSubsectionKeys().iterator();
+		while(defIterator.hasNext())
+		{
+			String key = (String)defIterator.next();
+//			PropertyBundle def = (PropertyBundle)defIterator.next();
+			String className = exceptionPagesSection.getValue(key + ".class", "org.ijsberg.iglu.SomeException").toString();
+			Class exceptionClass;
+			try
+			{
+				exceptionClass = Class.forName(className);
+			}
+			catch(Throwable t)
+			{
+				throw new ServletException("filter " + filterName + " can not handle exception '" + className + "'", t);
+			}
+			exceptionPages.put(exceptionClass, new ExceptionHandlingSettings(
+					exceptionPagesSection.getValue(key + ".page", "error.html").toString(),
+					exceptionPagesSection.getIndex(key + ".loglevel", StandardApplication.levelString, Application.CRITICAL)));
+
+
+		}
+//	}
+	}*/
+
+/*	private void obtainRealmReference(FilterConfig conf)
+	{
+		String realmId = conf.getInitParameter("realm_id");
+		if (realmId == null)
+		{
+			//obtain realm from context (put there by web server component or context listener)
+			realmId = (String) conf.getServletContext().getAttribute("realm_id");
+		}
+		if (realmId != null)
+		{
+			realm = application.getRealm(realmId);
+		}
+		if (realm == null)
+		{
+			realm = application.getPrimaryDomainRealm();
+		}
+		if (realm == null)
+		{
+			throw new ConfigurationException("realm for filter " + filterName +
+					" (acting as entry point) can not be determined");
+		}
+	}       */
+
+
+	/**
+	 * Must handle all incoming http requests.
+	 * Contains hooks for request and session management.
+	 *
+	 * @param servletRequest
+	 * @param servletResponse
+	 * @throws ServletException
+	 * @throws IOException
+	 */
+	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws ServletException, IOException
+	{
+		servletRequest.setCharacterEncoding("UTF-8");
+		httpRequest.set(servletRequest);
+		httpResponse.set(servletResponse);
+
+		Request appRequest = null;
+		try
+		{
+//			String sessionToken = ServletSupport.getSessionTokenFromCookie(servletRequest);
+			String sessionToken = ServletSupport.getCookieValue(servletRequest, SESSION_TOKEN_KEY);
+			String userId = ServletSupport.getCookieValue(servletRequest, USER_ID_KEY);
+			if("".equals(sessionToken))
+			{
+				sessionToken = null;
+			}
+			if("".equals(userId))
+			{
+				userId = null;
+			}
+  //FIXME
+//			appRequest = application.bindRequest(this);
+
+			if(accessManager != null) {
+				appRequest = accessManager.bindRequest(this);
+//				System.out.println("request bound!! " + appRequest);
+				Session session = appRequest.resolveSession(sessionToken, userId);
+			}
+
+
+			if (this.syncUserPrefs &&  appRequest.getTimesEntered() == 0)
+			{
+				//pass user preferences here
+				ServletSupport.importCookieValues(servletRequest,  appRequest.getUserSettings());
+				ServletSupport.exportCookieValues(servletResponse,  appRequest.getUserSettings(), "/", userPrefsMaxAge);
+			}
+
+			//if a user logged in, the user id must be stored
+			if(userId == null)
+			{
+				User user = appRequest.getUser();
+				if(user != null)
+				{
+					storeSessionDataInCookie(USER_ID_KEY, user.getId(), servletResponse);
+				}
+			}
+
+			//role based access control
+	//		checkAccess(servletRequest,  appRequest);
+
+			//delegate request
+			chain.doFilter(servletRequest, servletResponse);
+		}
+		catch (Throwable t)//make sure user gets a controlled response
+		{
+			//is this the actual entry point or is this entry point wrapped?
+			if ( appRequest != null &&  appRequest.getTimesEntered() > 1)
+			{
+				//it's wrapped, so the exception must be thrown at the top entry point
+				ServletSupport.rethrow(t);
+			}
+			handleException(servletRequest, servletResponse, t);
+		}
+		finally
+		{
+/*			if(loggingEnabled)
+			{
+				application.log(new PageVisitLogEntry((HttpServletRequest) servletRequest));
+			}
+			application.releaseRequest();  */
+			if(accessManager != null) {
+				accessManager.releaseRequest();
+			}
+		}
+	}
+
+	/**
+	 *
+	 * @param req
+	 * @param request
+	 * @throws SecurityException
+	 */
+/*	private void checkAccess(ServletRequest req, Request request)
+	{
+		if(req instanceof HttpServletRequest)
+		{
+			String requestURI = ((HttpServletRequest)req).getRequestURI();
+			Iterator i = securityConstraints.keySet().iterator();
+			LOOP:
+			while(i.hasNext())
+			{
+				String urlPattern = (String)i.next();
+				if(PatternMatchingSupport.valueMatchesWildcardExpression(requestURI, urlPattern))
+				{
+					User user = request.getUser();
+					List roles = (List)securityConstraints.get(urlPattern);
+					Iterator j = roles.iterator();
+					while(j.hasNext())
+					{
+						String role = (String)j.next();
+						if(user != null && user.hasRole(role))
+						{
+							break LOOP;
+						}
+					}
+					throw new SecurityException("user must have one of roles {" + CollectionSupport.format(roles, ", ") + "} to access " + requestURI);
+				}
+			}
+		}
+	}  */
+
+
+	/**
+	 * Is invoked in case an exception or error occurred in a servlet that was not handled by
+	 * the implementating code
+	 * <p/>
+	 * An attempt is made to redirect the request to an URL defined in the configuration
+	 * at section "SERVLET_EXCEPTION_REDIRECTS", key "unhandled_error"
+	 * <p/>
+	 * Override this method to implement your own error / exception handling
+	 *
+	 * @param request
+	 * @param response
+	 * @param cause
+	 */
+	public void handleException(ServletRequest request, ServletResponse response, Throwable cause) throws IOException, ServletException
+	{
+		List messageStack = new ArrayList();
+		messageStack.add("Request: " + request);
+		messageStack.add("Remote address: " + request.getRemoteAddr());
+		messageStack.add("Remote host: " + request.getRemoteHost());
+
+		//unwrap exception
+		while(/*cause instanceof ServletException && */cause.getCause() != null)
+		{
+			messageStack.add("Exception with message: " + cause.getMessage());
+			messageStack.add(cause.getStackTrace()[0].toString());
+
+			cause = cause.getCause();
+		}
+		//all servlets are responsible for handling all possible situations
+		//so an exception handled here is a critical one
+		if(cause instanceof ServletRequestAlreadyRedirectedException)
+		{
+			return;
+		}
+
+  /*
+		ArrayList testClasses = ReflectionSupport.getAllSuperclassesFromClass(cause.getClass());
+		testClasses.add(0, cause.getClass());
+		Iterator i = testClasses.iterator();
+		while(i.hasNext())
+		{
+			Class exceptionClass = (Class)i.next();
+			if(exceptionPages.containsKey(exceptionClass))
+			{
+				ExceptionHandlingSettings settings = (ExceptionHandlingSettings)exceptionPages.get(exceptionClass);
+				if(settings != null)
+				{
+					if(settings.loglevel > Application.UNDEFINED)
+					{
+						Environment.log(new ExtendedMessage(settings.loglevel, "exception handled in http-filter " + filterName + "\n" + CollectionSupport.format(messageStack, "\n"), cause));
+					}
+					ServletSupport.redirect(settings.redirectPage, request, response);
+					return;
+				}
+			}
+		}
+		System.out.println(new LogEntry(Level.CRITICAL, "unable to handle exception in http-filter " + filterName + "\n" + CollectionSupport.format(messageStack, "\n"), cause));
+    */
+		//print error to screen
+		if(this.printUnhandledExceptions)
+		{
+			if(!response.isCommitted())
+			{
+				ServletSupport.printException(response, "An exception occurred for which no exception page is defined.\n" +
+						"Make sure you do so if your application is in a production environment.\n" +
+						"(in section [" + exceptionPagesSectionId + "])" +
+						"\n\n" + CollectionSupport.format(messageStack, "\n"), cause);
+			}
+			else
+			{
+				System.out.println(new LogEntry(Level.CRITICAL, "exception handled in http-filter " + filterName + " can not be printed: response already committed"));
+			}
+		}
+	}
+
+
+
+	/**
+	 * Is invoked when the servlet runner shuts down
+	 */
+	public void destroy()
+	{
+	}
+
+
+
+
+}
+
---
+++ src/main/java/org/ijsberg/iglu/server/http/servlet/UploadServlet.java
@@ -0,0 +1,141 @@
+/*
+ * Copyright 2011 Jeroen Meetsma
+ *
+ *
+ * This file is part of Iglu.
+ *
+ * Iglu is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Iglu is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Iglu.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.ijsberg.iglu.server.http.servlet;
+
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
+/**
+ */
+public class UploadServlet extends HttpServlet
+{
+	private String uploaddir;
+
+	/**
+	 * @param conf
+	 * @throws ServletException
+	 */
+	public void init(ServletConfig conf) throws ServletException
+	{
+		super.init(conf);
+		uploaddir = getInitParameter("upload_directory");
+		if (uploaddir == null)
+		{
+			throw new ServletException("upload directory must be specified as servlet parameter upload_directory");
+		}
+		File uploadDirFile = new File(uploaddir);
+		if (!uploadDirFile.exists())
+		{
+			boolean success = uploadDirFile.mkdirs();
+			if (!success)
+			{
+				throw new ServletException("upload directory '" + uploaddir + "' does not exist and can not be created");
+			}
+		}
+		if (!uploadDirFile.isDirectory())
+		{
+			throw new ServletException("upload directory '" + uploaddir + "' is not a directory");
+		}
+	}
+
+
+
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException
+	{
+		res.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = res.getWriter();
+		writeBrowsePage(req, out);
+	}
+
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException
+	{
+		res.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = res.getWriter();
+
+		if (req != null && req.getContentType() != null && req.getContentType().startsWith("multipart/form-data"))
+		{
+			try
+			{
+				//FIXME
+				//ServletSupport.readMultipartUpload(req, uploaddir);
+/*				Enumeration e = req.getAttributeNames();
+				while (e.hasMoreElements())
+				{
+					String name = (String) e.nextElement();
+					Object attr = req.getAttribute(name);
+//					context.setProperty(new Property(name, new Value(attr)));
+				}*/
+				writeResultPage(out, "Thanks!");
+			}
+			catch (Exception ioe)//FIXME
+			{
+				//Environment.log(ioe);
+				writeResultPage(out, "Upload failed with message: " + ioe.getMessage());
+			}
+		}
+		else
+		{
+			doGet(req, res);
+		}
+	}
+
+
+	public static void writeBrowsePage(HttpServletRequest req, PrintWriter out)
+	{
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>file upload</title>");
+		out.println("</head>");
+
+		out.println("<body>");
+
+		out.println("<form ACTION=\"" + req.getServletPath() + "\" METHOD=\"POST\" ENCTYPE=\"multipart/form-data\">");
+		out.println("<input TYPE=\"FILE\" NAME=\"example\">");
+		out.println("<input TYPE=\"SUBMIT\" NAME=\"button\" VALUE=\"Upload\">");
+		out.println("</form>");
+
+		out.println("</body>");
+		out.println("</html>");
+	}
+
+
+	public static void writeResultPage(PrintWriter out, String message)
+	{
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>file upload</title>");
+		out.println("</head>");
+
+		out.println("<body>");
+
+		out.println("<p>" + message + "</p>");
+
+		out.println("</body>");
+		out.println("</html>");
+	}
+}
